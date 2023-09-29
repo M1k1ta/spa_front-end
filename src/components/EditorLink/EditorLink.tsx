92edd73e653e9b1e5b0eca5f87730c7fe3b993ac
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-09-29 15:06:34 +0300

    link default

--- src/components/EditorLink/EditorLink.tsx
+++ src/components/EditorLink/EditorLink.tsx
@@ -2,5 +2,5 @@ import { DecoratorComponentProps } from 'contenido';
 import React from 'react';
 
 export const EditorLink: React.FC<DecoratorComponentProps> = ({ href, children }) => {
-  return <a href={href || '/'}>{children}</a>;
+  return <a href={href || '/spa_front-end/#/'}>{children}</a>;
 };
